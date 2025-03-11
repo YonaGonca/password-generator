23a5264d2efdb3d00c328287ae9d76563732e78c
YonaGonca <ygoncalves5a@gmail.com>
2025-03-11 11:43:55 +0000

    -Project finished, copy to clipboard function added

---
+++ README.md
@@ -0,0 +1,50 @@
+# Password Generator
+
+## Description
+
+This is a random password generator developed with HTML, CSS, and JavaScript. It allows generating customized passwords with different character types and configurable lengths.
+
+## Features
+
+- Random password generation.
+- Options to include:
+  - Numbers (0-9)
+  - Lowercase letters (a-z)
+  - Uppercase letters (A-Z)
+  - Special characters (@, €, #, £, $, §, %, &)
+- Configurable password length.
+- Copy password to clipboard with a single click.
+- Attractive and easy-to-use interface.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript (ES6)
+
+## Installation and Usage
+
+1. Clone this repository or download the files manually.
+2. Open the `index.html` file in your preferred browser.
+3. Configure the desired options and press the "Generate" button to create a password.
+4. Click the copy icon to save the password to the clipboard.
+
+## Project Structure
+
+```
+password-generator/
+│-- index.html
+│-- styles/
+│   ├── password.css
+│-- scripts/
+│   ├── password.js
+```
+
+## Screenshot
+
+![Password Generator UI](assets/password.png)
+
+## License
+
+This project is licensed under the MIT License.
+
--- scripts/password.js
+++ scripts/password.js
@@ -5,7 +5,7 @@
 const numbers = "0123456789"
 const lowerLetters = "abcdefghijklmnopqrstuvwxyz"
 const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const specials = "!@€#£$§%&*+-<>;,.:_~^«»"
+const specials = "@€#£$§%&"
 
 //Functions
 
@@ -40,11 +40,31 @@ function generatePassword() {
     let charSet = createCharset();
     let numberOfChar = parseInt(document.getElementById("numberOfChar").value);
     if (charSet.length === 0) {
-        document.getElementById("result").innerText = "Selecciona al menos un tipo de carácter";
+        document.getElementById("result").innerText = "Select at least one character type";
         return;
     }
     let password = createPassword(numberOfChar, charSet);
-    document.getElementById("result").innerText = "Contraseña: " + password;
-}
+    document.getElementById("result").innerHTML = `${password}<i class="fa-solid fa-copy" id="icon" onclick="copyToClipboard()"></i>`;}
+
+function copyToClipboard() {
+
+    let copyText = document.getElementById("result").innerText;
+    let icon = document.getElementById("icon");
+      
+    navigator.clipboard.writeText(copyText).then(() => {
+        icon.classList.remove("fa-copy");
+        icon.classList.add("fa-check");
+
+        // Restaurar el icono después de 2 segundos
+        setTimeout(() => {
+            icon.classList.remove("fa-check");
+            icon.classList.add("fa-copy");
+        }, 2000);
+    }).catch(err => {
+        console.error("Error copying text: ", err);
+    });
+  } 
+
+
 
 
